7d8b2efb6ac8f939b7a208d2555cb4771fc3eb78
necroman <castilclansiggku@gmail.com>
2025-02-28 04:06:57 +0700

    Create shorten.js

---
+++ functions/shorten.js
@@ -0,0 +1,14 @@
+exports.handler = async (event) => {
+  const { url } = JSON.parse(event.body);
+  if (!url) {
+    return { statusCode: 400, body: "URL is required" };
+  }
+
+  const slug = Math.random().toString(36).substr(2, 6);
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      shortUrl: `https://your-site.netlify.app/${slug}`
+    })
+  };
+};
