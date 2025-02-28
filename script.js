2ba706015143eba7d66d802856ad8487ce633ed8
necroman <castilclansiggku@gmail.com>
2025-02-28 07:54:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,21 +14,26 @@ const firebaseConfig = {
   appId: "1:630396087951:web:73c2b84dc7c610d797cafa"
 };
 
+// Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const database = firebase.database(app);
 
+// Function to shorten URL
 function shortenURL() {
     const urlInput = document.getElementById("urlInput").value;
     const shortId = generateShortId();
-    const shortUrl = `https://usalink.netlify.app/${shortId}`;
-
+    
+    // Save the URL to Firebase with the generated shortId
     firebase.database().ref('urls/' + shortId).set({
         originalUrl: urlInput
     });
 
+    // Display the shortened URL
+    const shortUrl = `https://usalink.netlify.app/${shortId}`;
     document.getElementById("shortenedURL").textContent = shortUrl;
 }
 
+// Function to generate random short ID
 function generateShortId() {
-    return Math.random().toString(36).substring(2, 8); // Generate a random short ID
+    return Math.random().toString(36).substring(2, 8); // Generate random short ID
 }
