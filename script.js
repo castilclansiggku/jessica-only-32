e865ae0e5f5e3b015c373f6ff085e3cc28825fdb
necroman <castilclansiggku@gmail.com>
2025-02-28 07:33:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "YOUR_DATABASE_URL",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDQhyhUOeqKcF8-6jb1U8xK12n0VymOptI",
+  authDomain: "linkkuu.firebaseapp.com",
+  databaseURL: "https://linkkuu-default-rtdb.firebaseio.com",
+  projectId: "linkkuu",
+  storageBucket: "linkkuu.firebasestorage.app",
+  messagingSenderId: "630396087951",
+  appId: "1:630396087951:web:73c2b84dc7c610d797cafa"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
