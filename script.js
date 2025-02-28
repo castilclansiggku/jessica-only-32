857b114818994f13f5c27f3710d2f19ffc9cc919
necroman <castilclansiggku@gmail.com>
2025-02-28 08:24:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -34,14 +34,4 @@ function shortenURL() {
 function generateShortId() {
     return Math.random().toString(36).substring(2, 8); // Generate random short ID
 }
-const shortId = window.location.href.split('/').pop();
-firebase.database().ref('urls/' + shortId).once('value').then(function(snapshot) {
-    const originalUrl = snapshot.val()?.originalUrl;
-    if (originalUrl) {
-        window.location.href = originalUrl;  // Redirect ke URL asli
-    } else {
-        window.location.href = 'https://usalink.netlify.app/';  // Redirect default jika tidak ditemukan
-    }
-}).catch(function(error) {
-    console.error('Error redirecting:', error);
-});
+
